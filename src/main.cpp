0972e5af0e82cbddfe6bf8aaa6eb33e3e2c13404
DESKTOP-PIKAMVA\ringo <ringo@ringobell.co.kr>
2024-08-16 18:05:06 +0900

    rgconfig 추가 및 1차 테스트

--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,108 @@ String pwSec;
 // MAx size is 51 bytes for frame: 
 // {"ssidPrim":"","pwPrim":"","ssidSec":"","pwSec":""}
 // + 4 x 32 bytes for 2 SSID's and 2 passwords
-StaticJsonBuffer<200> jsonBuffer;
+StaticJsonBuffer<400> jsonBuffer;
+
+enum RGConfigType {
+	RGCONFIGTYPE_SWITCH,
+    RGCONFIGTYPE_SEEKBAR,
+    RGCONFIGTYPE_SPINNER,
+    RGCONFIGTYPE_NUMBER,
+    RGCONFIGTYPE_TEXT,
+};
+
+typedef struct RGConfigOption {
+	char o[16];
+} RGConfigOption;
+
+class RGConfig {
+public:
+	RGConfigType type;
+	int min;
+	int max;
+	char summary[32];
+	RGConfigOption *options;
+	int options_count;
+
+	RGConfig(RGConfigType type, int min, int max, char summary[32], RGConfigOption *options, int options_count) {
+		this->type = type;
+		this->min = min;
+		this->max = max;
+		strcpy(this->summary, summary);
+		this->options = options;
+		this->options_count = options_count;
+	}
+};
+
+class RGConfigInteger : public RGConfig {
+public:
+	int value;
+	int defaultValue;
+
+	RGConfigInteger(RGConfigType type, int value, int min, int max, int defaultValue, char summary[32], RGConfigOption *options, int options_count)
+		: RGConfig(type, min, max, summary, options, options_count)
+	{
+		Serial.print("v ");
+		Serial.print((uint32_t)(&this->value), HEX);
+		Serial.print(value);
+		this->value = value;
+		Serial.print(" tv ");
+		Serial.print((uint32_t)(&this->value), HEX);
+		Serial.println(this->value);
+		this->defaultValue = defaultValue;
+	}
+};
+
+class RGConfigString : public RGConfig {
+public:
+	char value[8];
+	char defaultValue[8];
+
+	RGConfigString(RGConfigType type, char value[8], int min, int max, char defaultValue[8], char summary[32], RGConfigOption *options, int options_count)
+		: RGConfig(type, min, max, summary, options, options_count)
+	{
+		strcpy(this->value, value);
+		strcpy(this->defaultValue, defaultValue);
+	}
+};
+
+String RGConfigTypeToString(RGConfigType type) {
+	String s = "Unknown";
+	switch(type) {
+	case RGCONFIGTYPE_SWITCH: s = "Switch"; break;
+    case RGCONFIGTYPE_SEEKBAR: s = "SeekBar"; break;
+    case RGCONFIGTYPE_SPINNER: s = "Spinner"; break;
+    case RGCONFIGTYPE_NUMBER: s = "Number"; break;
+    case RGCONFIGTYPE_TEXT: s = "Text"; break;
+	}
+	return s;
+}
+
+RGConfigOption rgco_spinner[2] = {
+	"Monday", "Tuesday"
+};
+const int rgco_spinner_count = sizeof(rgco_spinner) / sizeof(RGConfigOption);
+
+RGConfigInteger rgci_array[] {
+	RGConfigInteger(RGCONFIGTYPE_SWITCH, 0, 0, 1, 1, (char*)"switch Example", NULL, 0),
+	RGConfigInteger(RGCONFIGTYPE_SEEKBAR, 50, 0, 100, 50, (char*)"seekBar Example", NULL, 0 ),
+	RGConfigInteger(RGCONFIGTYPE_SPINNER, 1, 0, 2, 1, (char*)"spinner Example", rgco_spinner, rgco_spinner_count ),
+	RGConfigInteger(RGCONFIGTYPE_NUMBER, 10, 0, 10, 1, (char*)"number Example", NULL, 0 ),
+};
+
+RGConfigString rgcs_array[] {
+	RGConfigString(RGCONFIGTYPE_TEXT, (char*)"test", 0, 1, (char*)"default", (char*)"text Example", NULL, 0 ),
+};
+
+RGConfig* rgc_array[] = {
+	&rgci_array[0],
+	&rgci_array[1],
+	&rgci_array[2],
+	&rgci_array[3],
+	&rgcs_array[0],
+};
+const int rgc_array_count = sizeof(rgc_array) / sizeof(RGConfig*);
+
 
 /** Callback for receiving IP address from AP */
 void gotIP(arduino_event_id_t event) {
@@ -148,7 +249,49 @@ void connectWiFi() {
 }
 
 const int BUFFER_SIZE = 4096;
-uint8_t bleReadBuffer[BUFFER_SIZE];
+uint8_t ble_read_buffer[BUFFER_SIZE];
+uint8_t ble_write_buffer[BUFFER_SIZE];
+uint16_t ble_read_count;
+uint16_t ble_write_count;
+uint8_t ble_state = 100;
+uint8_t ble_state_timer100ms;
+String ble_write_string;
+String ble_read_string;
+int ble_config_index;
+
+void BleSerial_decode(uint8_t *value, uint32_t value_size)
+{
+    // Decode data    
+	int keyIndex = 0;
+    Serial.print("Received over BLESerial: ");
+    for (int index = 0; index < value_size; index ++) {
+        value[index] = (char) value[index] ^ (char) apName[keyIndex];
+        if (index < 10) {
+            Serial.print(value[index], HEX);
+        }
+        keyIndex++;
+        if (keyIndex >= strlen(apName)) keyIndex = 0;
+    }
+    Serial.print(" size ");
+    Serial.println(value_size);
+}
+
+void BleSerial_encode(uint8_t *value, uint32_t value_size)
+{
+    // encode the data
+    int keyIndex = 0;
+    Serial.print("Transmit over BLESerial: ");
+    for (int index = 0; index < value_size; index ++) {
+        if (index < 10) {
+            Serial.print(value[index], HEX);
+        }
+        value[index] = (char) value[index] ^ (char) apName[keyIndex];
+        keyIndex++;
+        if (keyIndex >= strlen(apName)) keyIndex = 0;
+    }
+    Serial.print(" size ");
+    Serial.println(value_size);
+}
 
 // Task for reading BLE Serial
 void ReadBLESerialTask(void *e)
@@ -157,14 +300,17 @@ void ReadBLESerialTask(void *e)
     {
         if (BleSerial_available())
         {
-			// onWrite
-            auto count = BleSerial_readBytes(bleReadBuffer, BUFFER_SIZE);
+            size_t count = BleSerial_readBytes(ble_read_buffer, BUFFER_SIZE);
 			if (count == 0) {
 				continue;
 			}
-			Serial.println("Received over BLESerial: " + String((char *)&bleReadBuffer[0]));			
-			char *value = (char*)bleReadBuffer;
-
+			BleSerial_decode(ble_read_buffer, count);
+			ble_read_buffer[count] = '\0';
+			ble_read_string = String((char*)ble_read_buffer);
+			Serial.print("rs ");
+			Serial.println(ble_read_string);
+			/*
+			char *value = (char*)ble_read_buffer;
 			// Decode data
 			int keyIndex = 0;
 			for (int index = 0; index < count; index ++) {
@@ -172,13 +318,17 @@ void ReadBLESerialTask(void *e)
 				keyIndex++;
 				if (keyIndex >= strlen(apName)) keyIndex = 0;
 			}
-
-			/** Json object for incoming data */
+			
+			// Json object for incoming data 
 			JsonObject& jsonIn = jsonBuffer.parseObject((char *)&value[0]);
 			if (jsonIn.success()) {
 				bool write = false;
 				if (jsonIn.containsKey("write")) {
 					write = jsonIn["write"].as<bool>();
+				}
+				bool enumerate = false;
+				if (jsonIn.containsKey("enumerate")) {
+					enumerate = jsonIn["enumerate"].as<bool>();
 				}
 				if (write) {
 					if (jsonIn.containsKey("ssidPrim") &&
@@ -229,14 +379,28 @@ void ReadBLESerialTask(void *e)
 				} else {
 					Serial.println("BLESerial onRead request");
 					String wifiCredentials;
-
-					/** Json object for outgoing data */
+					// Json object for outgoing data 
 					JsonObject& jsonOut = jsonBuffer.createObject();
-					jsonOut["write"] = false;
-					jsonOut["ssidPrim"] = ssidPrim;
-					jsonOut["pwPrim"] = pwPrim;
-					jsonOut["ssidSec"] = ssidSec;
-					jsonOut["pwSec"] = pwSec;
+					if (enumerate) {
+						//s = "{\"type\":\"Text\", \"value\": \"test\", \"min\": 0, \"max\": 10, \"defaultValue\" : \"default\", \"summary\":\"text Example\", \"options\": []}";
+						jsonOut["write"] = false;
+						jsonOut["enumerate"] = true;
+						JsonArray& jaConfigs = jsonOut.createNestedArray("configs");
+						JsonObject& joConfig = jaConfigs.createNestedObject();
+						joConfig["type"] = "Text";
+						joConfig["value"] = "";
+						joConfig["min"] = 0;
+						joConfig["max"] = 32;
+						joConfig["defaultValue"] = "";
+						joConfig["summary"] = "Primary AP";
+					} else {
+
+						jsonOut["write"] = false;
+						jsonOut["ssidPrim"] = ssidPrim;
+						jsonOut["pwPrim"] = pwPrim;
+						jsonOut["ssidSec"] = ssidSec;
+						jsonOut["pwSec"] = pwSec;
+					}
 					// Convert JSON object into a string
 					jsonOut.printTo(wifiCredentials);
 
@@ -255,14 +419,121 @@ void ReadBLESerialTask(void *e)
 				Serial.println("Received invalid JSON");
 			}
 			jsonBuffer.clear();
+			*/
         }
-        delay(20);
+		switch(ble_state) {
+		case 0:
+			break;
+
+		case 100:
+		{
+			if (ble_read_string == "")
+				break;
+
+			JsonObject& jo = jsonBuffer.parseObject(ble_read_string);
+			ble_read_string = "";
+			if (jo.success() == false) 
+				break;
+
+			if (jo.containsKey("read") == false)
+				break;
+
+			if (jo["read"].as<String>() == "config_count")
+			{
+				ble_state = 110;
+				break;		
+			}
+			if (jo["read"].as<String>() == "config_index")
+			{
+				ble_config_index = jo["config_index"].as<int>();
+				ble_state = 120;
+				break;		
+			}
+			break;
+		}
+
+		case 110:
+		{
+			// Json object for outgoing data 
+			JsonObject& jo = jsonBuffer.createObject();
+			jo["read"] = "config_count";
+			jo["config_count"] = rgc_array_count;
+
+			ble_write_string = ""; jo.printTo(ble_write_string);
+			jsonBuffer.clear();
+			ble_write_count = ble_write_string.length();
+			Serial.print("ws ");
+			Serial.println(ble_write_string);			
+			memcpy(ble_write_buffer, (void*)&ble_write_string[0], ble_write_string.length());
+			BleSerial_encode(ble_write_buffer, ble_write_count);
+			BleSerial_write(ble_write_buffer, ble_write_count);
+			ble_state = 100;
+			break;
+		}
+
+		case 120:
+		{
+			// Json object for outgoing data 
+			JsonObject& jo = jsonBuffer.createObject();
+			jo["read"] = "config_index";
+			if (ble_config_index >= rgc_array_count) {
+				jo["config_index"] = -1;
+			} else {
+				jo["config_index"] = ble_config_index;
+				if (rgc_array[ble_config_index]->type == RGCONFIGTYPE_SWITCH ||
+					rgc_array[ble_config_index]->type == RGCONFIGTYPE_SEEKBAR ||
+					rgc_array[ble_config_index]->type == RGCONFIGTYPE_SPINNER ||
+					rgc_array[ble_config_index]->type == RGCONFIGTYPE_NUMBER) {
+					//RGConfigInteger* rgci = (RGConfigInteger*)&rgc[ble_config_index];
+					RGConfigInteger &rgci = (RGConfigInteger &)(*rgc_array[ble_config_index]);
+					Serial.print("v ");
+					Serial.println(rgci.value);
+					jo["type"] = RGConfigTypeToString(rgci.type);
+					jo["value"] = rgci.value;
+					jo["min"] = rgci.min;
+					jo["max"] = rgci.max;
+					jo["defaultValue"] = rgci.defaultValue;
+					jo["summary"] = rgci.summary;
+					JsonArray &ja = jo.createNestedArray("options");
+					for(int i = 0; i < rgci.options_count; i++) {
+						ja.add(rgci.options[i].o);
+					}
+				} else if (rgc_array[ble_config_index]->type == RGCONFIGTYPE_TEXT) {
+					RGConfigString &rgcs = (RGConfigString &)(*rgc_array[ble_config_index]);
+					jo["type"] = RGConfigTypeToString(rgcs.type);
+					jo["value"] = rgcs.value;
+					jo["min"] = rgcs.min;
+					jo["max"] = rgcs.max;
+					jo["defaultValue"] = rgcs.defaultValue;
+					jo["summary"] = rgcs.summary;
+					JsonArray &ja = jo.createNestedArray("options");
+					for(int i = 0; i < rgcs.options_count; i++) {
+						ja.add(rgcs.options[i].o);
+					}
+				}
+			}
+
+			ble_write_string = ""; jo.printTo(ble_write_string);
+			jsonBuffer.clear();
+			ble_write_count = ble_write_string.length();
+			Serial.print("ws ");
+			Serial.println(ble_write_string);			
+			memcpy(ble_write_buffer, (void*)&ble_write_string[0], ble_write_string.length());
+			BleSerial_encode(ble_write_buffer, ble_write_count);
+			BleSerial_write(ble_write_buffer, ble_write_count);
+			ble_state = 100;
+			break;
+		}
+		break;
+		}
+        delay(10);
     }
 }
 
 void setup() {
 	// Initialize Serial port
-	Serial.begin(115200);
+	//Serial.begin(115200);
+	Serial.begin(115200, SERIAL_8N1, 18, 17);
 
 	// Send some device info
 	Serial.print("Build: ");
@@ -305,7 +576,23 @@ void setup() {
 			connectWiFi();
 		}
 	}
-
+	/*
+	for(int i = 0; i < 4; i++)
+	{
+		RGConfigInteger *rgci = (RGConfigInteger *)(rgc_array[i]);
+		Serial.print("v ");
+		Serial.print((uint32_t)(&rgci->value), HEX);
+		Serial.println(rgci->value);
+	}
+	
+	for(int i = 4; i < 5; i++)
+	{
+		RGConfigString *rgcs = (RGConfigString *)(rgc_array[i]);
+		Serial.print("v ");
+		Serial.print((uint32_t)(&rgcs->value), HEX);
+		Serial.println(rgcs->value);
+	}
+	*/
     // Start tasks
     xTaskCreate(ReadBLESerialTask, "ReadBLESerialTask", 10240, NULL, 1, NULL);
 }
